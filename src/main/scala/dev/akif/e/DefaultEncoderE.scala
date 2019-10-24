2264b2e9541fad7fd710d5993d9414fc766acc82
Mehmet Akif Tütüncü <m.akif.tutuncu@gmail.com>
2019-10-24 13:32:49 +0300

    Fix DefaultEncoderE so it produces valid Json String

--- src/main/scala/dev/akif/e/DefaultEncoderE.scala
+++ src/main/scala/dev/akif/e/DefaultEncoderE.scala
@@ -4,17 +4,23 @@ object DefaultEncoderE extends EncoderE[String] {
   override def encode(e: E): String = {
     val strings = List(
       s""""code":${e.code}""",
-      if (e.name.isEmpty) "" else s""""name":"${e.name}"""",
-      if (e.message.isEmpty) "" else s""""message":"${e.message}"""",
-      e.cause.fold("")(c => s""""cause":"${c.getMessage}""""),
-      if (e.data.isEmpty) "" else s""""data":${e.data.map({case (k, v) => s""""$k":"$v""""}).mkString("{", ",", "}")}"""
+      if (e.name.isEmpty) "" else s""""name":"${escape(e.name)}"""",
+      if (e.message.isEmpty) "" else s""""message":"${escape(e.message)}"""",
+      e.cause.fold("")(c => s""""cause":"${escape(c.getMessage)}""""),
+      if (e.data.isEmpty) "" else s""""data":${e.data.map({case (k, v) => s""""${escape(k)}":"${escape(v)}""""}).mkString("{", ",", "}")}"""
     )
 
-    val builder = strings.foldLeft(new StringBuilder()) {
-      case (sb, "") => sb
-      case (sb, s)  => sb.append(s).append(",")
+    val size = strings.size
+
+    val (_, builder) = strings.foldLeft(1 -> new StringBuilder("{")) {
+      case ((i, sb), "") if i == size && sb.last == ',' => (i + 1) -> sb.dropRight(1)
+      case ((i, sb), "")                                => (i + 1) -> sb
+      case ((i, sb), s) if i < size                     => (i + 1) -> sb.append(s).append(",")
+      case ((i, sb), s)                                 => (i + 1) -> sb.append(s)
     }
 
-    builder.insert(0, "{").append("}").toString()
+    builder.append("}").toString()
   }
+
+  private def escape(s: String): String = s.replaceAll("\"", "\\\\\"")
 }
