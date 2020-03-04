686baa11568f04b71a4d823edcedb4bacef4a3d9
Scala Steward <me@scala-steward.org>
2020-03-04 02:51:12 +0100

    Update zio to 1.0.0-RC18-1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,7 +5,7 @@ object Dependencies {
   lazy val circeParser    = "io.circe"             %% "circe-parser"      % "0.13.0"
   lazy val gson           = "com.google.code.gson"  % "gson"              % "2.8.6"
   lazy val playJson       = "com.typesafe.play"    %% "play-json"         % "2.8.1"
-  lazy val zio            = "dev.zio"              %% "zio"               % "1.0.0-RC17"
+  lazy val zio            = "dev.zio"              %% "zio"               % "1.0.0-RC18-1"
   lazy val jUnit          = "org.junit.jupiter"     % "junit-jupiter"     % "5.6.0" % Test
   lazy val jUnitInterface = "net.aichler"           % "jupiter-interface" % "0.8.3" % Test
   lazy val scalaTest      = "org.scalatest"        %% "scalatest"         % "3.1.1" % Test
