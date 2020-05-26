9c75dcba45138fcc868ff10a211626baa155e64a
Murat Ayaz <murat.ayaz@vngrs.com>
2020-05-26 21:27:14 +0300

    update play example according to E V2

--- examples/e-play-example/app/dev/akif/eplayexample/common/Repository.scala
+++ examples/e-play-example/app/dev/akif/eplayexample/common/Repository.scala
@@ -2,15 +2,12 @@ package dev.akif.eplayexample.common
 
 import java.sql.Connection
 
-import dev.akif.eplayexample.common.implicits._
-import e.scala.Maybe
-import e.zio.MaybeZ
+import e.zio._
+import e._
 
 trait Repository {
   val db: DB.Def
 
-  protected def run[A](f: Connection => A): MaybeZ[A] =
-    Maybe.catching(t => Errors.database.message("Database error!").cause(t)) {
-      db.withConnection(f)
-    }.toMaybeZ
+  protected def run[A](f: Connection => A): EIO[A] =
+    db.withConnection(f).catching(t => Errors.database.message("Database error!").cause(t.toE)).toEIO
 }
