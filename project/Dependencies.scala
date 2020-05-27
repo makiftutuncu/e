cdc468e1c08703e547580073ac23081ec05b19a0
Scala Steward <me@scala-steward.org>
2020-05-27 18:38:24 +0200

    Update play-json to 2.9.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,7 +4,7 @@ object Dependencies {
   lazy val circeCore      = "io.circe"             %% "circe-core"        % "0.13.0"
   lazy val circeParser    = "io.circe"             %% "circe-parser"      % "0.13.0"
   lazy val gson           = "com.google.code.gson"  % "gson"              % "2.8.6"
-  lazy val playJson       = "com.typesafe.play"    %% "play-json"         % "2.8.1"
+  lazy val playJson       = "com.typesafe.play"    %% "play-json"         % "2.9.0"
   lazy val zio            = "dev.zio"              %% "zio"               % "1.0.0-RC17"
   lazy val jUnit          = "org.junit.jupiter"     % "junit-jupiter"     % "5.6.2" % Test
   lazy val jUnitInterface = "net.aichler"           % "jupiter-interface" % "0.8.3" % Test
