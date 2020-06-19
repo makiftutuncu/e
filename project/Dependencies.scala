635770229694c3b1a8000f0f81a3ae5323ecb41d
Scala Steward <me@scala-steward.org>
2020-06-19 05:35:31 +0200

    Update scalatest to 3.2.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -8,5 +8,5 @@ object Dependencies {
   lazy val zio            = "dev.zio"              %% "zio"               % "1.0.0-RC17"
   lazy val jUnit          = "org.junit.jupiter"     % "junit-jupiter"     % "5.6.2" % Test
   lazy val jUnitInterface = "net.aichler"           % "jupiter-interface" % "0.8.3" % Test
-  lazy val scalaTest      = "org.scalatest"        %% "scalatest"         % "3.1.2" % Test
+  lazy val scalaTest      = "org.scalatest"        %% "scalatest"         % "3.2.0" % Test
 }
