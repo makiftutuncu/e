dbdcf6b7328325324540dc1bc9143bfba0736f73
Scala Steward <me@scala-steward.org>
2021-02-03 17:04:33 +0100

    Update kotlin-compiler-embeddable to 1.4.30

--- project/Settings.scala
+++ project/Settings.scala
@@ -6,7 +6,7 @@ import sbt._
 
 object Settings {
   lazy val javaVersion         = "1.8"
-  lazy val latestKotlinVersion = "1.4.21-2"
+  lazy val latestKotlinVersion = "1.4.30"
   lazy val latestScalaVersion  = "2.13.2"
 
   lazy val crossCompiledScalaVersions = Seq("2.12.12", latestScalaVersion)
