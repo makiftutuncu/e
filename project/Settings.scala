12fba031bbdae7a7becad50a9827898f699810c9
Scala Steward <me@scala-steward.org>
2021-09-14 23:35:00 +0200

    Update scala-library to 2.12.15

--- project/Settings.scala
+++ project/Settings.scala
@@ -9,7 +9,7 @@ object Settings {
   lazy val latestKotlinVersion = "1.4.32"
   lazy val latestScalaVersion  = "2.13.5"
 
-  lazy val crossCompiledScalaVersions = Seq("2.12.13", latestScalaVersion)
+  lazy val crossCompiledScalaVersions = Seq("2.12.15", latestScalaVersion)
 
   lazy val commonSettings = Seq(
     resolvers += Resolver.jcenterRepo
