acaf2f5069a329f7e9b41d96eb6c06cf57eb80b1
Scala Steward <me@scala-steward.org>
2022-04-13 06:48:11 +0200

    Update zio, zio-test, zio-test-sbt to 1.0.14

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,12 +6,12 @@ object Dependencies {
   lazy val circeParser    = "io.circe"             %% "circe-parser" % "0.14.1"
   lazy val gson           = "com.google.code.gson"  % "gson"         % "2.9.0"
   lazy val playJson       = "com.typesafe.play"    %% "play-json"    % "2.9.2"
-  lazy val zio            = "dev.zio"              %% "zio"          % "1.0.13"
+  lazy val zio            = "dev.zio"              %% "zio"          % "1.0.14"
 
   lazy val jUnit           = "org.junit.jupiter"  % "junit-jupiter"     % "5.8.2" % Test
   lazy val jUnitInterface  = "net.aichler"        % "jupiter-interface" % "0.10.0" % Test
   lazy val mUnit           = "org.scalameta"     %% "munit"             % "0.7.29" % Test
   lazy val mUnitScalaCheck = "org.scalameta"     %% "munit-scalacheck"  % "0.7.29" % Test
-  lazy val zioTest         = "dev.zio"           %% "zio-test"          % "1.0.13" % Test
-  lazy val zioTestSBT      = "dev.zio"           %% "zio-test-sbt"      % "1.0.13" % Test
+  lazy val zioTest         = "dev.zio"           %% "zio-test"          % "1.0.14" % Test
+  lazy val zioTestSBT      = "dev.zio"           %% "zio-test-sbt"      % "1.0.14" % Test
 }
