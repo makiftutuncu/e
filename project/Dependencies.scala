c182ea2513e314c6d807c6b51ee609f1a4523277
Scala Steward <scala_steward@virtuslab.com>
2022-07-06 19:06:48 +0000

    Update cats-core to 2.8.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,7 +1,7 @@
 import sbt._
 
 object Dependencies {
-  lazy val catsCore       = "org.typelevel"        %% "cats-core"    % "2.7.0"
+  lazy val catsCore       = "org.typelevel"        %% "cats-core"    % "2.8.0"
   lazy val circeCore      = "io.circe"             %% "circe-core"   % "0.14.2"
   lazy val circeParser    = "io.circe"             %% "circe-parser" % "0.14.2"
   lazy val gson           = "com.google.code.gson"  % "gson"         % "2.9.0"
