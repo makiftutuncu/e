5ff167d038dae117cd8114383f937c3598d467da
Mehmet Akif Tütüncü <m.akif.tutuncu@gmail.com>
2022-07-19 10:30:13 +0300

    Update ZIO to 2

--- e-zio/src/test/scala/e/ZIOTest.scala
+++ e-zio/src/test/scala/e/ZIOTest.scala
@@ -1,12 +1,12 @@
 package e
 
-import e.scala._
-import zio.test._
-import zio.test.environment.TestEnvironment
-import zio.test.Assertion._
 import e.ezio._
+import e.scala._
+import zio.{Scope, ZLayer}
+import zio.test.Assertion._
+import zio.test._
 
-object ZIOTest extends DefaultRunnableSpec {
+object ZIOTest extends ZIOSpecDefault {
   private val divideByZero: E = E.name("divideError").message("Cannot divide by 0!")
 
   class Divider {
@@ -20,48 +20,41 @@ object ZIOTest extends DefaultRunnableSpec {
 
   private val divider: Divider = new Divider
 
-  private def divide(a: Int, b: Int): REIO[Divider, Int] = REIO.accessM[Divider](_.divide(a, b))
+  private def divide(a: Int, b: Int): REIO[Divider, Int] = REIO.serviceWithZIO[Divider](_.divide(a, b))
 
-  private val allTests: ZSpec[Divider, E] = suite("e-zio")(
-    testM("failed EIO") {
-      assertM(divider.divide(2, 0).run) {
-        fails {
+  override def spec: Spec[TestEnvironment with Scope, Any] =
+    suite("e-zio")(
+      test("failed EIO") {
+        assertZIO(divider.divide(2, 0).exit) {
+          fails {
+            equalTo {
+              divideByZero.data("input", 2)
+            }
+          }
+        }
+      },
+      test("successful EIO") {
+        assertZIO(divider.divide(4, 2)) {
           equalTo {
-            divideByZero.data("input", 2)
+            2
+          }
+        }
+      },
+      test("failed REIO") {
+        assertZIO(divide(3, 0).exit) {
+          fails {
+            equalTo {
+              divideByZero.data("input", 3)
+            }
+          }
+        }
+      },
+      test("successful REIO") {
+        assertZIO(divide(6, 2)) {
+          equalTo {
+            3
           }
         }
       }
-    },
-
-    testM("successful EIO") {
-      assertM(divider.divide(4, 2)) {
-        equalTo {
-          2
-        }
-      }
-    },
-
-    testM("failed REIO") {
-      assertM(divide(3, 0).run) {
-        fails {
-          equalTo {
-            divideByZero.data("input", 3)
-          }
-        }
-      }
-    },
-
-    testM("successful REIO") {
-      assertM(divide(6, 2)) {
-        equalTo {
-          3
-        }
-      }
-    }
-  )
-
-  override def spec: ZSpec[TestEnvironment, E] = allTests.provide(divider)
-
-  // ZIO test fails as `java.lang.NoSuchMethodError: zio.ZIO.raceWith` due to it's default timeout configuration 🤷🏻
-  override def aspects: List[TestAspect[Nothing, TestEnvironment, Nothing, Any]] = List.empty
+    ).provide(ZLayer.succeed(divider))
 }
